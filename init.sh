63b0aee0d8de863bcac417dcb26ced613851e9b7
S03D4-164 <S03D4-164@users.noreply.github.com>
2020-04-06 22:00:26 +0900

    wget ip2loc db in init

--- init.sh
+++ init.sh
@@ -5,3 +5,7 @@ mkdir -p $ESDIR/data
 mkdir -p $ESDIR/config && cp es/elasticsearch.yml $ESDIR/config
 chmod -R 777 $ESDIR
 sudo sysctl -w vm.max_map_count=262144
+
+wget https://download.ip2location.com/lite/IP2LOCATION-LITE-DB1.IPV6.BIN.ZIP \
+&& unzip IP2LOCATION-LITE-DB1.IPV6.BIN.ZIP -x *.TXT \
+&& mv IP2LOCATION-LITE-DB1.IPV6.BIN wgeteer/routes 
