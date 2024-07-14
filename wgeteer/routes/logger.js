f2d3d4da7e3df4e27dbf72db14cc529b4eacce9a
S03D4-164 <12199393+S03D4-164@users.noreply.github.com>
2024-07-14 16:00:43 +0900

    refactor wgeteer logger

--- wgeteer/routes/logger.js
+++ wgeteer/routes/logger.js
@@ -1,16 +1,16 @@
-const { createLogger, format, transports } = require('winston');
+const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, label, prettyPrint } = format;
 
-const prettyJson = format.printf(info => {
+const prettyJson = format.printf((info) => {
   if (info.message.constructor === Object) {
-    info.message = JSON.stringify(info.message, null, 4)
+    info.message = JSON.stringify(info.message, null, 4);
   }
-  return `${info.timestamp} [${info.level}] ${info.message}`
-})
+  return `${info.timestamp} [${info.level}] ${info.message}`;
+});
 
 module.exports = createLogger({
   format: combine(
-    label({ label: 'wgeteer' }),
+    label({ label: "wgeteer" }),
     timestamp(),
     prettyPrint(),
     format.splat(),
@@ -18,23 +18,23 @@ module.exports = createLogger({
     prettyJson,
   ),
   transports: [
-      new transports.File({
-          level: 'info',
-          filename: './public/logs/ppengo.log',
-          handleExceptions: true,
-          json: false,
-          maxsize: 5242880, //5MB
-          maxFiles: 5,
-          colorize: false,
-          timestamp:true
-        }),
-      new transports.Console({
-          level: 'debug',
-          handleExceptions: true,
-          json: false,
-          colorize: true,
-          timestamp:true
-      })
+    new transports.File({
+      level: "info",
+      filename: "./public/logs/ppengo.log",
+      handleExceptions: true,
+      json: false,
+      maxsize: 5242880, //5MB
+      maxFiles: 5,
+      colorize: false,
+      timestamp: true,
+    }),
+    new transports.Console({
+      level: "debug",
+      handleExceptions: true,
+      json: false,
+      colorize: true,
+      timestamp: true,
+    }),
   ],
-  exitOnError: false
+  exitOnError: false,
 });
