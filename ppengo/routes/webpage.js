ca19740655da917d3a453fcd6592b867c65e4dfb
S03D4-164 <12199393+S03D4-164@users.noreply.github.com>
2024-07-14 15:58:33 +0900

    refactor webpage in routes

--- ppengo/routes/webpage.js
+++ ppengo/routes/webpage.js
@@ -1,164 +1,193 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var paginate = require('express-paginate');
+var paginate = require("express-paginate");
 
-const { Parser } = require('@json2csv/plainjs');
+const { Parser } = require("@json2csv/plainjs");
 
-var Diff = require('diff');
+var Diff = require("diff");
 
-const Webpage = require('./models/webpage');
-const Request = require('./models/request');
-const Response = require('./models/response');
-const Website = require('./models/website');
+const Webpage = require("./models/webpage");
+const Request = require("./models/request");
+//const Response = require('./models/response');
+const Website = require("./models/website");
 
-const logger = require('./logger')
-const prediction = require('./prediction')
+const logger = require("./logger");
+//const prediction = require('./prediction')
 
-router.get('/', function(req, res) {
-  var search = []
-  if(typeof req.query.input !== 'undefined' && req.query.input){
-    search.push({"input": req.query.input});
+router.get("/", function (req, res) {
+  var search = [];
+  if (typeof req.query.input !== "undefined" && req.query.input) {
+    search.push({ input: req.query.input });
   }
-  if(typeof req.query.rinput !== 'undefined' && req.query.rinput){
-    search.push({"input": new RegExp(RegExp.escape(req.query.rinput))});
-  }
-
-  if(typeof req.query.title !== 'undefined' && req.query.title){
-    search.push({"title": new RegExp(req.query.title)});
-  }
-  if(typeof req.query.url !== 'undefined' && req.query.url){
-    search.push({"url": req.query.url});
-  }
-  if(typeof req.query.rurl !== 'undefined' && req.query.rurl){
-    search.push({"url":new RegExp(
-      req.query.rurl.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&')
-    )});
+  if (typeof req.query.rinput !== "undefined" && req.query.rinput) {
+    search.push({ input: new RegExp(RegExp.escape(req.query.rinput)) });
   }
 
-  if(typeof req.query.source !== 'undefined' && req.query.source){
-    search.push({"content": new RegExp(req.query.source)});
+  if (typeof req.query.title !== "undefined" && req.query.title) {
+    search.push({ title: new RegExp(req.query.title) });
   }
-  if(typeof req.query.ip !== 'undefined' && req.query.ip){
-      search.push({"remoteAddress.ip":new RegExp(req.query.ip)});
+  if (typeof req.query.url !== "undefined" && req.query.url) {
+    search.push({ url: req.query.url });
   }
-  if(typeof req.query.country !== 'undefined' && req.query.country){
-    search.push({"remoteAddress.geoip.country":new RegExp(req.query.country)});
+  if (typeof req.query.rurl !== "undefined" && req.query.rurl) {
+    search.push({
+      url: new RegExp(req.query.rurl.replace(/[\\^$.*+?()[\]{}|]/g, "\\$&")),
+    });
   }
-  if(typeof req.query.status !== 'undefined' && req.query.status){
+
+  if (typeof req.query.source !== "undefined" && req.query.source) {
+    search.push({ content: new RegExp(req.query.source) });
+  }
+  if (typeof req.query.ip !== "undefined" && req.query.ip) {
+    search.push({ "remoteAddress.ip": new RegExp(req.query.ip) });
+  }
+  if (typeof req.query.country !== "undefined" && req.query.country) {
+    search.push({
+      "remoteAddress.geoip.country": new RegExp(req.query.country),
+    });
+  }
+  if (typeof req.query.status !== "undefined" && req.query.status) {
     //search.push({"$where": `/${req.query.status}/.test(this.status)`});
-    search.push({"status": req.query.status});
+    search.push({ status: req.query.status });
   }
   let verbose;
-  if(typeof req.query.verbose !== 'undefined' && req.query.verbose){
+  if (typeof req.query.verbose !== "undefined" && req.query.verbose) {
     verbose = true;
   }
 
-  if(typeof req.query.csv !== 'undefined' && req.query.csv){
+  if (typeof req.query.csv !== "undefined" && req.query.csv) {
     var find = Webpage.find();
-    if(search.length)find = find.and(search);
+    if (search.length) find = find.and(search);
     find.sort("-createdAt").then((webpage) => {
-      var fields = ['createdAt', 'input', 'title', 'error', 'status', 'remoteAddress.ip', 'remoteAddress.reverse', 'remoteAddress.geoip', 'wappalyzer', 'securityDetails.issuer', 'securityDetails.validFrom', 'securityDetails.validTo', 'url'];
-      const opts ={ withBOM:true, fields:fields };
+      var fields = [
+        "createdAt",
+        "input",
+        "title",
+        "error",
+        "status",
+        "remoteAddress.ip",
+        "remoteAddress.reverse",
+        "remoteAddress.geoip",
+        "wappalyzer",
+        "securityDetails.issuer",
+        "securityDetails.validFrom",
+        "securityDetails.validTo",
+        "url",
+      ];
+      const opts = { withBOM: true, fields: fields };
       const parser = new Parser(opts);
-      const csv = parser.parse(websites);
+      const csv = parser.parse(webpage);
 
-      res.setHeader('Content-disposition', 'attachment; filename=webpages.csv');
-      res.setHeader('Content-Type', 'text/csv; charset=UTF-8');
+      res.setHeader("Content-disposition", "attachment; filename=webpages.csv");
+      res.setHeader("Content-Type", "text/csv; charset=UTF-8");
       res.send(csv);
-    })
-  }else{
-    var query = search.length?{"$and":search}:{};
+    });
+  } else {
+    var query = search.length ? { $and: search } : {};
 
     Webpage.paginate(
-      query, {
-      sort:{"createdAt":-1},
-      page: req.query.page,
-      limit: req.query.limit,
-      lean: true,
-    }, function(err, result) {
+      query,
+      {
+        sort: { createdAt: -1 },
+        page: req.query.page,
+        limit: req.query.limit,
+        lean: true,
+      },
+      function (err, result) {
         //console.log(query, err, result)
         let pages = {};
-        if (result) pages = paginate.getArrayPages(req)(
-          5, result.totalPages, req.query.page
-        );
-        res.render('pages', {
+        if (result)
+          pages = paginate.getArrayPages(req)(
+            5,
+            result.totalPages,
+            req.query.page,
+          );
+        res.render("pages", {
           title: "Pages",
           search: req.query,
           result,
           verbose,
           pages: pages,
-          err: err
+          err: err,
         });
-    });
-
+      },
+    );
   }
 });
 
-router.get('/:id', async function(req, res, next) {
-  const id = req.params.id;  
-  var webpage = await Webpage.findById(id)
-    .then((document) => {
-      return document;
+router.get("/:id", async function (req, res) {
+  const id = req.params.id;
+  var webpage = await Webpage.findById(id).then((document) => {
+    return document;
   });
-  
+
   //const pred = await prediction.imgPrediction(webpage.thumbnail);
 
   var previous, diff;
-  if (webpage.content){
+  if (webpage.content) {
     previous = await Webpage.find({
-      "input":webpage.input,
-      "createdAt":{$lt: webpage.createdAt},
+      input: webpage.input,
+      createdAt: { $lt: webpage.createdAt },
       //"status":{$ge: 0}
-    }).sort("-createdAt").lean()
-    .then((document) => {
-      //console.log(document.length);
-      return document;
-    });
-    if (previous.length){
+    })
+      .sort("-createdAt")
+      .lean()
+      .then((document) => {
+        //console.log(document.length);
+        return document;
+      });
+    if (previous.length) {
       previous = previous[0];
-      if (previous.content && webpage.content){
-        diff =  Diff.createPatch("", previous.content, webpage.content, previous._id, webpage._id) 
+      if (previous.content && webpage.content) {
+        diff = Diff.createPatch(
+          "",
+          previous.content,
+          webpage.content,
+          previous._id,
+          webpage._id,
+        );
       }
     }
   }
   //console.log(diff);
 
   var search = [];
-  if(typeof req.query.rurl !== 'undefined' && req.query.rurl){
+  if (typeof req.query.rurl !== "undefined" && req.query.rurl) {
     //search.push({"url":new RegExp(RegExp.escape(req.query.rurl))});
-    search.push({"url":new RegExp(req.query.rurl)});
-
+    search.push({ url: new RegExp(req.query.rurl) });
   }
-  if(typeof req.query.source !== 'undefined' && req.query.source){
+  if (typeof req.query.source !== "undefined" && req.query.source) {
     //search.push({"text": new RegExp(RegExp.escape(req.query.source))});
-    search.push({"text": new RegExp(req.query.source)});
+    search.push({ text: new RegExp(req.query.source) });
   }
-  if(typeof req.query.status !== 'undefined' && req.query.status){
-    search.push({"$where": `/${req.query.status}/.test(this.status)`});
+  if (typeof req.query.status !== "undefined" && req.query.status) {
+    search.push({ $where: `/${req.query.status}/.test(this.status)` });
   }
 
   const result = await Request.paginate(
-      {"webpage": id}, {
-      sort:{"createdAt":1},
+    { webpage: id },
+    {
+      sort: { createdAt: 1 },
       page: req.query.page,
       limit: req.query.limit,
       lean: true,
-      populate:{
+      populate: {
         path: "response",
-        select: "_id remoteAddress status securityDetails yara payload text"
+        select: "_id remoteAddress status securityDetails yara payload text",
       },
-    }, function(err, result) {
-      return result
+    },
+    function (err, result) {
+      return result;
       //return paginate.getArrayPages(req)(5, result.totalPages, req.query.page)
-    });
-    console.log(result.docs)
+    },
+  );
+  //console.log(result.docs)
 
-    var pages =  paginate.getArrayPages(req)(5, result.totalPages, req.query.page)
+  var pages = paginate.getArrayPages(req)(5, result.totalPages, req.query.page);
 
   logger.debug(req.query, search);
 
-
-/*
+  /*
   var responses;
   if(search.length){
     const find = await Response.find({"webpage":id}).and(search)
@@ -173,21 +202,22 @@ router.get('/:id', async function(req, res, next) {
   } 
 */
 
-  var website = await Website.findOne({"url":webpage.input})
-  .lean().then((document) => {
-    return document;
-  });
+  var website = await Website.findOne({ url: webpage.input })
+    .lean()
+    .then((document) => {
+      return document;
+    });
 
-  res.render('page', { 
-        webpage,
-        result,
-        pages,
-        //responses,
-        website,
-        previous:previous,
-        diff,
-        search:req.query,
-        title: "Request",
+  res.render("page", {
+    webpage,
+    result,
+    pages,
+    //responses,
+    website,
+    previous: previous,
+    diff,
+    search: req.query,
+    title: "Request",
   });
 });
 
