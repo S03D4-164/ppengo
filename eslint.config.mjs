9a0e5751379a722793f0b87aa61c84024ff6903c
S03D4-164 <12199393+S03D4-164@users.noreply.github.com>
2025-02-15 11:14:37 +0900

    feat: 🎸 husky fook for commit

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
